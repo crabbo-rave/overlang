205c599932075753f64faa75e3b454ad212fcc0b
crab <92041779+crabbo-rave@users.noreply.github.com>
2022-05-15 11:25:57 -0400

    Parser work

--- overlang/Parser.fs
+++ overlang/Parser.fs
@@ -1,9 +1,19 @@
 ﻿module Parser
 
+open System
 open FParsec
+
+type UserState = unit // doesn't have to be unit, of course
+type Parser<'t> = Parser<'t, UserState>
 
 let test p str =
     match run p str with 
     | Success(result, _, _)   -> printfn "Success: %A" result
     | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
 
+let ws = spaces
+
+let oconst : Parser<_> = pfloat .>> ws 
+let oid : Parser<_> =
+    let isID c = isLetter c || Char.IsPunctuation c 
+    many1SatisfyL isID "identifier" .>> ws
--- overlang/Program.fs
+++ overlang/Program.fs
@@ -2,6 +2,6 @@
 open FParsec
 
 let main =
-    test pfloat "1.25"
-    test pfloat "1.25E 3"
+    test oconst "12.4"
+    test oid "hello!f?"
     0
